7428e2d201a5670f28a778687b39742779c19e37
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-14 16:44:36 +0530

    prgm
    good

---
+++ digitssumnbnj.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+
+    printf("Number of digits: %d", count);
+
+getch();
+}
